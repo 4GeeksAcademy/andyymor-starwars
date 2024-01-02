c8731664cbaa4f945e9da885f5b209e78bd7f85b
andyymor <61852990+andyymor@users.noreply.github.com>
2024-01-02 23:46:55 +0000

    set up components

---
+++ src/js/views/characterDescription.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+
+
+export const CharacterDescription = () => {
+	const { store, actions } = useContext(Context);
+	const {id} = useParams();
+	return (
+		<div className="container">
+			
+		</div>
+	);
+};
